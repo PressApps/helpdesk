abdd880aebe916f7408a33ad0ace2e16246c6186
Ivan <ichromjak@gmail.com>
2014-10-31 19:41:28 +0100

    icon mind

--- page.php
+++ page.php
@@ -2,4 +2,7 @@
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
-<h2><span class="icon-picture"></span> This is kj hgkjhgjk vhjgvhj</h2>
+<h2><span class="icon-document"></span> This is kj hgkjhgjk vhjgvhj <span class="icon-envelope icon-list"></span></h2>
+
+<h2><span class="icon-Air-Balloon"></span> This is kj hgkjhgjk vhjgvhj</h2>
+
